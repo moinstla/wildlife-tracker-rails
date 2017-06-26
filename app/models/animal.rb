5d9eb423b520a335d2ae9d52f74c3d86910ef00c
Dana Weiss and Monique St. Laurent <monique@>
2017-06-26 13:52:55 -0700

    animal class

---
+++ app/models/animal.rb
@@ -0,0 +1,8 @@
+class Animal < ActiveRecord::Base
+  validates :name, :presence => true
+  validates :species, :presence => true
+  validates :coloring, :presence => true
+  validates :markings, :presence => true
+  validates :age, :presence => true
+  validates :tag, :presence => true
+end
--- db/migrate/20170626203054_create_animals.rb
+++ db/migrate/20170626203054_create_animals.rb
@@ -1,6 +1,14 @@
 class CreateAnimals < ActiveRecord::Migration[5.0]
   def change
     create_table :animals do |t|
+      t.column :name, :string
+      t.column :species, :string
+      t.column :coloring, :string
+      t.column :markings, :string
+      t.column :age, :integer
+      t.column :tag, :integer
+
+      t.timestamps
     end
   end
 end
